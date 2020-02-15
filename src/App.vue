403f6499d6a9209cb4f3d0d9eeed04804f7704b4
NAN LU <lvnan.airchina@gmail.com>
2020-02-15 15:26:19 +0800

    Add files via upload
    Add a new, since git remote failed.

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <nav-bar></nav-bar>
+    <home :imgDetail="imgDetail"></home>
+    
+   
+    
+    
+  </div>
+</template>
+
+<script>
+
+import NavBar from './components/NavBar.vue'
+import Home from './views/Home.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    
+    NavBar,
+    Home,
+  },
+  
+  data: () => ({
+    imgDetail: {
+      mv: {
+        imgUrl: 'https://img1.doubanio.com/view/photo/m/public/p2585603719.webp',
+        imgTitle: 'DWAY!'
+      },
+      ad: {
+        imgUrl: 'https://img3.doubanio.com/view/photo/m/public/p2586104370.webp',
+        imgTitle: 'Panda'
+      },
+      stage: {
+        imgUrl: 'https://img3.doubanio.com/view/photo/m/public/p2586104390.webp',
+        imgTitle: 'stage'
+      }
+      
+      
+    }
+  })
+  
+}
+</script>
+
+<style>
+
+</style>
